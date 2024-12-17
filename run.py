249012a287fdfae27b36a71baec004bdbc78f22b
awaisnazir08 <owaisnazir2228@gmail.com>
2024-12-17 11:19:23 +0500

    add flask-cors in run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS  # Import CORS
 from app.routes import UserAccountManagement
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
     UserAccountManagement(app)
     return app
 
